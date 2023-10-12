fd749a61a216b0fd8d48307d648a9960959b3cc4
Jannah Anwar <jannah.anwar@outlok.com>
2023-10-12 22:03:37 +0100

    finished goal model

--- server/src/main/java/com/hackathon/server/models/Goal.java
+++ server/src/main/java/com/hackathon/server/models/Goal.java
@@ -1,4 +1,34 @@
 package com.hackathon.server.models;
 
 public class Goal {
+
+    private Long id;
+
+    private String goal;
+
+    public Goal() {
+    }
+
+    public Goal(Long id, String goal) {
+        this.id = id;
+        this.goal = goal;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGoal() {
+        return goal;
+    }
+
+    public void setGoal(String goal) {
+        this.goal = goal;
+    }
+
 }
+
