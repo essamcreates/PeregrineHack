b0a1b0967ddcba458a6feeab8fbf8092da6105ab
Jannah Anwar <jannah.anwar@outlok.com>
2023-10-12 21:20:22 +0100

    started editing useResponse model

--- server/src/main/java/com/hackathon/server/models/UserResponse.java
+++ server/src/main/java/com/hackathon/server/models/UserResponse.java
@@ -1,4 +1,17 @@
 package com.hackathon.server.models;
 
 public class UserResponse {
+
+    private Long id;
+
+    //private USER USER
+
+    private String userPrompt;
+
+    private String responseText;
+
+    private LocalDate dateTime;
+
+    public UserResponse() {
+    }
 }
