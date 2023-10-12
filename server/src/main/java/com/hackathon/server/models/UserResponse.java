b29629717740cf8fe77c9121a4eda6d0b83b4439
Jannah Anwar <jannah.anwar@outlok.com>
2023-10-12 21:21:00 +0100

    finished user response model but did not add a user yet

--- server/src/main/java/com/hackathon/server/models/UserResponse.java
+++ server/src/main/java/com/hackathon/server/models/UserResponse.java
@@ -14,4 +14,43 @@ public class UserResponse {
 
     public UserResponse() {
     }
+
+    public UserResponse(Long id, String userPrompt, String responseText, LocalDate dateTime) {
+        this.id = id;
+        this.userPrompt = userPrompt;
+        this.responseText = responseText;
+        this.dateTime = dateTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserPrompt() {
+        return userPrompt;
+    }
+
+    public void setUserPrompt(String userPrompt) {
+        this.userPrompt = userPrompt;
+    }
+
+    public String getResponseText() {
+        return responseText;
+    }
+
+    public void setResponseText(String responseText) {
+        this.responseText = responseText;
+    }
+
+    public LocalDate getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDate dateTime) {
+        this.dateTime = dateTime;
+    }
 }
