d9b1b059fd2ab2b54865ea4b7fe5cbaf21e101da
Callum BP <122090016+cbattenplowright@users.noreply.github.com>
2023-10-12 14:47:37 +0100

    Created AccessNeedService model

--- server/src/main/java/com/hackathon/server/services/AccessNeedService.java
+++ server/src/main/java/com/hackathon/server/services/AccessNeedService.java
@@ -1,4 +1,20 @@
 package com.hackathon.server.services;
 
-public class AccessNeedService {
+public enum AccessNeedService {
+
+    ADHD,
+    DYSLEXIA,
+    ASD,
+    ALZHEIMERS,
+    EPILEPSY,
+    BLINDNESS,
+    LOW_VISION,
+    COLOUR_BLINDNESS,
+    TUNNEL_VISION,
+    MACULAR_DEGENERATION,
+    RETINITIS_PIGMENTOSA,
+    DEAFNESS,
+    HARD_OF_HEARING,
+    AUDITORY_PROCESSING,
+    MOBILITY
 }
