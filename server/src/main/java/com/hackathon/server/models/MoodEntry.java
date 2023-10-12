99dc3a15024b9e434cc039a9479fd9b71c4b92d3
Jannah Anwar <jannah.anwar@outlok.com>
2023-10-12 21:18:31 +0100

    finished mood entry model

--- server/src/main/java/com/hackathon/server/models/MoodEntry.java
+++ server/src/main/java/com/hackathon/server/models/MoodEntry.java
@@ -10,5 +10,45 @@ public class MoodEntry {
     private LocalDateTime dateTime;
     private String emojiUnicode;
 
+    public MoodEntry() {
+    }
 
+    public MoodEntry(Long id, String mood, LocalDateTime dateTime, String emojiUnicode) {
+        this.id = id;
+        this.mood = mood;
+        this.dateTime = dateTime;
+        this.emojiUnicode = emojiUnicode;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMood() {
+        return mood;
+    }
+
+    public void setMood(String mood) {
+        this.mood = mood;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getEmojiUnicode() {
+        return emojiUnicode;
+    }
+
+    public void setEmojiUnicode(String emojiUnicode) {
+        this.emojiUnicode = emojiUnicode;
+    }
 }
