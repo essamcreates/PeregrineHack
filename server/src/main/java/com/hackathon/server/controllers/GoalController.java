11ac0181c911e768af55dbf09daae60d93eedd48
charlotteg1 <charlotte.gould3@gmail.com>
2023-10-13 17:30:11 +0100

    added goal, MH, mood controllers

--- server/src/main/java/com/hackathon/server/controllers/GoalController.java
+++ server/src/main/java/com/hackathon/server/controllers/GoalController.java
@@ -1,4 +1,56 @@
 package com.hackathon.server.controllers;
 
+import com.hackathon.server.models.Goal;
+import com.hackathon.server.models.dtos.UserGoalDTO;
+import com.hackathon.server.services.GoalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("careerGoals")
 public class GoalController {
+
+    @Autowired
+    GoalService goalService;
+
+    // GET mapping
+    @GetMapping
+    public ResponseEntity<List<Goal>> getAllGoals(){
+        return new ResponseEntity<>(goalService.getAllGoals(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Goal> getGoalById(@PathVariable Long id){
+        return new ResponseEntity<>(goalService.getGoalById(id), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{userId}")
+    public ResponseEntity<Optional<List<Goal>>> getGoalsByUser(@PathVariable Long userId){
+        Optional<List<Goal>> userGoals= goalService.getGoalsByUser(userId);
+        if(userGoals.isPresent()){
+            return new ResponseEntity<>(goalService.getGoalsByUser(userId).get(), HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping(value = "/{userId}")
+    public void createUserGoals(@RequestBody UserGoalDTO userGoalDTO, @PathVariable Long userId){
+        goalService.saveUserGoals(userGoalDTO, userId);
+    }
+
+
+
+
+
+//    GET getAllGoals
+//GET getGoalById
+//GET getAllGoalsByUser
+//POST createGoal
+//DELETE deleteGoal
 }
--- server/src/main/java/com/hackathon/server/services/GoalService.java
+++ server/src/main/java/com/hackathon/server/services/GoalService.java
@@ -1,4 +1,44 @@
 package com.hackathon.server.services;
 
+import com.hackathon.server.models.Goal;
+import com.hackathon.server.models.User;
+import com.hackathon.server.repositories.GoalRepository;
+import com.hackathon.server.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class GoalService {
+
+    @Autowired
+    GoalRepository goalRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    public List<Goal> getAllGoals() {
+        return goalRepository.findAll();
+    }
+
+    public Goal getGoalById(Long id) {
+        return goalRepository.findById(id).get();
+    }
+
+//    public Optional<Goal> getGoalsByUser(Long userId) {
+//        Optional<User> user = userRepository.findById(userId);
+//        if(user.isPresent()){
+//            return user.getCareerGoals();
+//        }else{
+//            return null;
+//        }
+//    }
+
+    public 
+
+
+
+
 }
