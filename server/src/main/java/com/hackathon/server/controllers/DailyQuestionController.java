19212f1f262ae428eab8c4496812c3c673321eca
Jannah Anwar <jannah.anwar@outlok.com>
2023-10-13 15:49:32 +0100

    started set up of dailyQuestionController

--- server/src/main/java/com/hackathon/server/controllers/DailyQuestionController.java
+++ server/src/main/java/com/hackathon/server/controllers/DailyQuestionController.java
@@ -1,4 +1,13 @@
 package com.hackathon.server.controllers;
 
+import com.hackathon.server.services.DailyQuestionService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping({"/dailyQuestions"})
 public class DailyQuestionController {
+
+    DailyQuestionService dailyQuestionService;
 }
