b85c123cc0639c76b0299868c5151455794dea6c
charlotteg1 <charlotte.gould3@gmail.com>
2023-10-16 16:45:07 +0100

    routes now working

--- front-end-client/src/Container/Container.jsx
+++ front-end-client/src/Container/Container.jsx
@@ -1,21 +1,27 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "../Components/HomePage/HomePage";
 import LoginPage from "../Components/LoginPage/LoginPage";
+import QuizPage from "../Components/QuizPage/QuizPage";
+import AccountRegistrationPage from "../Components/AccountRegistrationPage/AccountRegistrationPage";
+import ProfileCreationPage from "../Components/ProfileCreationPage/ProfileCreationPage";
+import ProfilePage from "../Components/ProfilePage/ProfilePage";
 
 const Container = () => {
 
     return (
         <div>
-        {/* <BrowserRouter>
+        <BrowserRouter>
+        <main>
             <Routes>
                 <Route path="/" element={<HomePage/>}></Route>
                 <Route path="/LoginPage" element={<LoginPage/>}></Route>
                 <Route path="/AccountRegistrationPage" element={<AccountRegistrationPage/>}></Route>
                 <Route path="/ProfileCreationPage" element={<ProfileCreationPage/>}></Route>
-                <Route path="/ProfilePage" element={<ProfileCreationPage/>}></Route>
+                <Route path="/ProfilePage" element={<ProfilePage/>}></Route>
                 <Route path="/QuizPage" element={<QuizPage/>}></Route>
             </Routes>
-        </BrowserRouter> */}
+        </main>
+        </BrowserRouter>
         </div>
     )
 }
