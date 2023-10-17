99cdfa18061e5a60e764c561c533f1db4908bb09
Callum BP <122090016+cbattenplowright@users.noreply.github.com>
2023-10-17 11:46:38 +0100

    created BigFiveQuestion

---
+++ server/src/main/java/com/hackathon/server/sentinoAPI/BigFiveQuestion.java
@@ -0,0 +1,38 @@
+package com.hackathon.server.sentinoAPI;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "big_five_questions")
+public class BigFiveQuestion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String question;
+
+    public BigFiveQuestion(){
+    }
+
+    public BigFiveQuestion(String question) {
+        this.question = question;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+}
