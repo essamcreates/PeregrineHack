3154719076659092ad6c60925b3f5caf2c7b981c
essamcreates <mrm.hussain@hotmail.co.uk>
2023-10-17 17:34:04 +0100

    made a start on ProfileCreationPage

--- front-end-client/src/Components/ProfileCreationPage/ProfileBox.jsx
+++ front-end-client/src/Components/ProfileCreationPage/ProfileBox.jsx
@@ -1,7 +1,50 @@
+import { useState } from "react";
+
 const ProfileBox = () => {
 
+    const [enteredJobRole, setEnteredJobRole] = useState ("")
+    const [enteredCareerGoals, setEnteredCareerGoals] = useState ("")
+    const [enteredDOB, setEnteredDOB] = useState ("")
+    const [ enteredGender, setEnteredGender] = useState ("")
+    const [ enteredAccessNeeds, setEnteredAccessNeeds] = useState ("")
+
+    const handleSignupClick = async(event) => {
+        event.preventDefault();
+        if (!enteredJobRole, !enteredCareerGoals, !enteredDOB, !enteredGender, !enteredAccessNeeds) {
+            alert("Please enter all fields")
+            // highlight fields that are left empty
+        } else {
+            let temp = {
+                DOB: enteredDOB,
+                Gender: enteredGender
+                // do we only need the above two as they will be the ones that need to be added to the database?
+            }
+        }
+
+    }
+
     return (
-        <></>
+        <div>
+            <form className="profileCreation" onSubmit={(event)=> {handleSignupClick(event)}}>
+            <label> Job Role/Title:</label>
+                <input className="input-box" type="text" value={enteredJobRole} onChange={(e)=>{setEnteredJobRole(e.target.value)}}/>
+                <br>
+                </br>
+                <label> Career Goals:</label>
+                <input className="input-box" type="text" value={enteredCareerGoals} onChange={(e)=>{setEnteredCareerGoals(e.target.value)}}/>
+                <br/>
+                <br/>
+                <label> Date of Birth:</label>
+                <input className="input-box" type="text" value={enteredDOB} onChange={(e)=>{setEnteredDOB(e.target.value)}}/>
+                <br>
+                </br>
+                <label> Gender:</label>
+                <input className="input-box" type="text" value={enteredGender} onChange={(e)=>{setEnteredGender(e.target.value)}}/>
+                <input type="submit" value="Add"/>
+
+            </form>
+
+        </div>
     )
 }
 export default ProfileBox;
--- front-end-client/src/Components/ProfileCreationPage/ProfileCreationPage.jsx
+++ front-end-client/src/Components/ProfileCreationPage/ProfileCreationPage.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+import ProfileBox from "./ProfileBox";
+
 const ProfileCreationPage = () => {
 
     return (
-        <></>
+        <div>
+            <h1>Welcome [user]!</h1>
+            <h2>I am Bob, your personal development coach. Please give me some information about yourself so I can get to know you!</h2>
+            {/* Profile creation bar */}
+            <ProfileBox />
+        </div>
     )
 }
 export default ProfileCreationPage;
