965a9fc6e5b708927c10d1aae2e6fc65fc867472
essamcreates <mrm.hussain@hotmail.co.uk>
2023-10-17 11:36:18 +0100

    added accountregistration

--- front-end-client/src/Components/AccountRegistrationPage/AccountRegistrationForm.jsx
+++ front-end-client/src/Components/AccountRegistrationPage/AccountRegistrationForm.jsx
@@ -1,7 +1,30 @@
 const AccountRegistrationForm = () => {
 
+    const [enteredEmail, setEnteredEmail] = useState ("")
+    const [enteredPassword, setEnteredPassword] = useState ("")
+    const [enteredName, setEnteredName] = useState ("")
+    const [enteredConfirmedPassword, setConfirmedPassword] = useState ("")
+    
     return (
-        <></>
+        <div>
+             <form className="signup-form" onSubmit={(event)=>{handlesignupClick(event)}}>
+             <label> First Name:</label>
+                <input className="input-box" type="text" value={enteredName} onChange={(e)=>{setEnteredName(e.target.value)}}/>
+                <br>
+                </br>
+                <label> Email:</label>
+                <input className="input-box" type="text" value={enteredEmail} onChange={(e)=>{setEnteredEmail(e.target.value)}}/>
+                <br/>
+                <br/>
+                <label> Password:</label>
+                <input className="input-box" type="text" value={enteredPassword} onChange={(e)=>{setEnteredPassword(e.target.value)}}/>
+                <br>
+                </br>
+                <label> Confirm Password:</label>
+                <input className="input-box" type="text" value={enteredConfirmedPassword} onChange={(e)=>{setConfirmedPassword(e.target.value)}}/>
+                <input type="submit" value="Sign up"/>
+            </form>
+        </div>
     )
 }
 export default AccountRegistrationForm;
--- front-end-client/src/Components/AccountRegistrationPage/AccountRegistrationPage.jsx
+++ front-end-client/src/Components/AccountRegistrationPage/AccountRegistrationPage.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+import AccountRegistrationForm from "./AccountRegistrationForm";
+
 const AccountRegistrationPage = () => {
 
-    return (
-        <></>
-    )
+
+
+            return (
+                <div >
+                    <h1>Account sign up</h1>
+                    <AccountRegistrationForm/>
+                </div>
+            )
+        
 }
 export default AccountRegistrationPage;
