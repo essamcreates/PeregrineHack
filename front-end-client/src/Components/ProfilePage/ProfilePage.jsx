c5d762f2caecb83180f2c1c1accdf4b741e52dee
TarekQMUL <t.u.ahmed@se21.qmul.ac.uk>
2023-10-18 15:10:44 +0100

    testing'

--- front-end-client/src/Components/ProfilePage/ProfilePage.jsx
+++ front-end-client/src/Components/ProfilePage/ProfilePage.jsx
@@ -1,24 +1,30 @@
 import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 const ProfilePage = ({currentUser}) => {
 
-    const [userScore, setUserScore] = useState();
+    const [userScore, setUserScore] = useState([]);
     const fetchUserScore = async () =>{
-        const response = await fetch('https://localhost:8080/personalityScores/'+currentUser.id);
+        const response = await fetch('https://localhost:8080/personalityScores/user/'+currentUser.id);
         const data = await response.json();
         setUserScore(data);
     }
+
+    useEffect(()=>{
+        fetchUserScore();
+
+    },[]);
 
     return (
         <div>
             <div>
                 <p>Your Big Five:</p>
 
-                userScore && (<p>Openness: {userScore.openness}</p>)
-                userScore && (<p>Conscientiousness: {userScore.conscientiousness}</p>)
-                userScore && (<p>Extraversion: {userScore.extraversion}</p>)
-                userScore && (<p>Agreeableness: {userScore.agreeableness}</p>)
-                userScore && (<p>Neuroticism: {userScore.neuroticism}</p>)
+                {/*userScore && (<p>Openness: {userScore.openness}</p>)*/}
+                {/*userScore && (<p>Conscientiousness: {userScore.conscientiousness}</p>)*/}
+                {/*userScore && (<p>Extraversion: {userScore.extraversion}</p>)*/}
+                {/*userScore && (<p>Agreeableness: {userScore.agreeableness}</p>)*/}
+                {/*userScore && (<p>Neuroticism: {userScore.neuroticism}</p>)*/}
 
             </div>
             <div>
