b2aa015231245be64eaf75e1200fcae1cfc8a064
TarekQMUL <t.u.ahmed@se21.qmul.ac.uk>
2023-10-18 17:36:09 +0100

    moved bigfive fetch to BigFiveDisplay page

--- front-end-client/src/Components/ProfilePage/ProfilePage.jsx
+++ front-end-client/src/Components/ProfilePage/ProfilePage.jsx
@@ -1,31 +1,12 @@
 import {Link} from "react-router-dom";
-import {useEffect, useState} from "react";
+import BigFiveDisplay from "./BigFiveDisplay.jsx";
 
 const ProfilePage = ({currentUser}) => {
-
-    const [userScore, setUserScore] = useState([]);
-    const fetchUserScore = async () =>{
-        const response = await fetch('http://localhost:8080/personalityScores/user/'+ currentUser.id);
-        const data = await response.json();
-        setUserScore(data);
-    }
-
-    useEffect(()=>{
-        fetchUserScore();
-
-    },[]);
 
     return (
         <div>
             <div>
-                <p>Your Big Five:</p>
-
-                {currentUser && userScore && (<p>Openness: {userScore.openness}</p>)}
-                {currentUser && userScore && (<p>Conscientiousness: {userScore.conscientiousness}</p>)}
-                {currentUser && userScore && (<p>Extraversion: {userScore.extraversion}</p>)}
-                {currentUser && userScore && (<p>Agreeableness: {userScore.agreeableness}</p>)}
-                {currentUser && userScore && (<p>Neuroticism: {userScore.neuroticism}</p>)}
-
+                <BigFiveDisplay currentUser={currentUser}/>
             </div>
             <div>
                 {currentUser && (<p>Hello {currentUser.name}</p>)}
