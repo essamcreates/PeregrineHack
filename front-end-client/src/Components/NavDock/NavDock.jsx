7d92d3e233545cf1ae11f325865e3fdfcbfa2e61
charlotteg1 <charlotte.gould3@gmail.com>
2023-10-18 11:03:15 +0100

    changed to bottm left

--- front-end-client/src/Components/NavDock/NavDock.jsx
+++ front-end-client/src/Components/NavDock/NavDock.jsx
@@ -7,14 +7,11 @@ const NavDock = ({currentUser}) => {
     const [navOpen, setNavOpen] = useState(false)
     const navigate = useNavigate();
     const location = useLocation();
-    // in container put navDock around pages that this will be shown  - done 
+
     // have shown in numerous pages like HomePage, Profile page etc - done NOTE: may want a user creation page link or to quiz.
-    // take the url to find the current page and exclude that from option- done
-    // ensure is logged in (then can show option to sign out)- done
-    // each box is an icon hover to get page name etc
-    // is a modal ?? - yes
-    // modal displays after 2 seconds of hover or on click on the button
-    // closes after 5 seconds or after button clicked again
+    // import icons
+    // move to bottom left
+    // apply css to button  nav-open-menu-button and nav-close-menu-button
 
     return (
         <div className="nav-dock">
@@ -25,13 +22,13 @@ const NavDock = ({currentUser}) => {
                     <div className="nav-dock-modal">
                         <div className="nav-dock-modal-content">
                         {location.pathname!=="/HomePage" && (<div>
-                            <button className="nav-icon-button" data-id="Home" onClick={()=>{navigate("/HomePage")}}>🏠</button>
+                            <button className="nav-icon-button" data-id="Home" onClick={()=>{setNavOpen(false);navigate("/HomePage")}}>🏠</button>
                         </div>)}
                         {location.pathname!=="/ProfilePage" && (<div>
-                            <button className="nav-icon-button" data-id="UserProfile" onClick={()=>{navigate("/ProfilePage")}}>👥</button>
+                            <button className="nav-icon-button" data-id="UserProfile" onClick={()=>{setNavOpen(false);navigate("/ProfilePage")}}>👥</button>
                         </div>)}
                         {location.pathname!=="/LoginPage" && (<div>
-                            <button className="nav-icon-button" data-id="Signout" onClick={()=>{navigate("/LoginPage")}}>👋🏼</button>
+                            <button className="nav-icon-button" data-id="Signout" onClick={()=>{setNavOpen(false);navigate("/LoginPage")}}>👋🏼</button>
                         </div>)}
                     </div>
                     </div>)}
