571df2f9041253d38a4f814338540319c05267f7
amelieboydshire <aboydshire10@gmail.com>
2023-10-19 15:59:18 +0100

    basic styling on login page

--- front-end-client/src/Components/LoginPage/LoginPage.jsx
+++ front-end-client/src/Components/LoginPage/LoginPage.jsx
@@ -8,26 +8,30 @@ const LoginPage = ({authenticateUser, currentUser, setCurrentUser}) => {
     const navigate = useNavigate();
 
     return (
-        <div class="min-h-screen flex items-center justify-center bg-red-300">
-            <div class="bg-white shadow-lg rounded-lg p-6">
-                {!currentUser && ( 
-                <>
-                <h1>User Login</h1>
-                <h2>Welcome to your personal development coach</h2>
-                <LoginForm authenticateUser={authenticateUser} currentUser={currentUser} setCurrentUser={setCurrentUser} />
-                </>
-                )}
-                {currentUser && (
-                <>
-                <h1>Sign Out</h1>
-                <LoginForm authenticateUser={authenticateUser} currentUser={currentUser} setCurrentUser={setCurrentUser} />
-                </>
-                )}
-                {!currentUser && (
-                <button onClick={() => {navigate("/AccountRegistrationPage")}}>Don't have an account, sign up</button>
-            )}
-            </div>
-        </div>
+        <div class="min-h-screen flex items-center justify-center bg-stone-100">
+  <div class="bg-white shadow-lg rounded-lg h-80 p-10">
+    {!currentUser && (
+      <>
+        {/* <h1>User Login</h1> */}
+        <h1 class="text-2x1 mb-10">Welcome to your personal development coach</h1>
+        <LoginForm authenticateUser={authenticateUser} currentUser={currentUser} setCurrentUser={setCurrentUser} />
+      </>
+    )}
+    {currentUser && (
+      <>
+        <h1>Sign Out</h1>
+        <LoginForm authenticateUser={authenticateUser} currentUser={currentUser} setCurrentUser={setCurrentUser} />
+      </>
+    )}
+
+    <div class="w-full flex justify-end">
+    {!currentUser && (
+      <button onClick={() => {navigate("/AccountRegistrationPage")}}>New user? Create an account</button>
+    )}
+    </div>
+  </div>
+</div>
+
     )
 }
 export default LoginPage;
