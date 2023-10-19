4ae528edad15cb3698d33d50e2f35b7fe151147f
amelieboydshire <aboydshire10@gmail.com>
2023-10-19 13:18:36 +0100

    centred LoginForm in box on the page

--- front-end-client/src/Components/LoginPage/LoginPage.jsx
+++ front-end-client/src/Components/LoginPage/LoginPage.jsx
@@ -8,19 +8,25 @@ const LoginPage = ({authenticateUser, currentUser, setCurrentUser}) => {
     const navigate = useNavigate();
 
     return (
-        <div className="w-screen h-screen bg-red-300">
-        {!currentUser && 
-        (<><h1>User Login</h1>
-        <h2> Welcome to your personal development coach</h2>
-        <LoginForm authenticateUser={authenticateUser} currentUser={currentUser} setCurrentUser={setCurrentUser}/>
-        </>)}
-        {/* sign out */}
-        {currentUser && (<>
-        <h1>SignOut</h1>
-        <LoginForm authenticateUser={authenticateUser} currentUser={currentUser} setCurrentUser={setCurrentUser}/>
-        </>)}
-        {/* will move the below button out of login box down the line when doing css */}
-        {!currentUser && (<button onClick={()=>{navigate("/AccountRegistrationPage")}}>Don't have an account, sign up</button>)}
+        <div class="min-h-screen flex items-center justify-center bg-red-300">
+            <div class="bg-white shadow-lg rounded-lg p-6">
+                {!currentUser && ( 
+                <>
+                <h1>User Login</h1>
+                <h2>Welcome to your personal development coach</h2>
+                <LoginForm authenticateUser={authenticateUser} currentUser={currentUser} setCurrentUser={setCurrentUser} />
+                </>
+                )}
+                {currentUser && (
+                <>
+                <h1>Sign Out</h1>
+                <LoginForm authenticateUser={authenticateUser} currentUser={currentUser} setCurrentUser={setCurrentUser} />
+                </>
+                )}
+                {!currentUser && (
+                <button onClick={() => {navigate("/AccountRegistrationPage")}}>Don't have an account, sign up</button>
+            )}
+            </div>
         </div>
     )
 }
