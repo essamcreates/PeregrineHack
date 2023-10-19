ce92db692cb9dc243424e99f98424fe2da6d770b
TarekQMUL <t.u.ahmed@se21.qmul.ac.uk>
2023-10-19 11:16:51 +0100

    profile photo uploads from db

--- front-end-client/src/Components/ProfilePage/ProfilePage.jsx
+++ front-end-client/src/Components/ProfilePage/ProfilePage.jsx
@@ -3,12 +3,16 @@ import BigFiveDisplay from "./BigFiveDisplay.jsx";
 
 const ProfilePage = ({currentUser}) => {
 
+
+
     return (
         <div>
             <div>
                 <BigFiveDisplay currentUser={currentUser}/>
             </div>
             <div>
+                <img src={`http://localhost:8080${currentUser.profilePictureURL}`} alt="User Profile Picture" />
+                {/*<img src={`http://localhost:8080/images/johnpfp.jpeg`} alt="User Profile Picture" />*/}
                 {currentUser && (<p>Hello {currentUser.name}</p>)}
             </div>
             <div>
