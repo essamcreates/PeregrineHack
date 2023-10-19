019265e3cd5aa247e3c56fc5234be10f8950a3db
Callum BP <122090016+cbattenplowright@users.noreply.github.com>
2023-10-19 18:43:23 +0100

    questions gets set with questions fetched from API

--- front-end-client/src/Components/QuizPage/Option.jsx
+++ front-end-client/src/Components/QuizPage/Option.jsx
@@ -1,38 +1,34 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 const Option = () => {
-
-    const [questions, setQuestions] = useState([]);
-    const fetchUserScore = async () => {
-        try {
-        const response = await fetch('http://localhost:8080/personalityQuestionnaire');
-        if (response.ok) {
+  const [questions, setQuestions] = useState([]);
+  const fetchUserScore = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/personalityQuestionnaire", {
+        method: "GET"
+      });
+      console.log(response);
+      if (response.status === 302) {
         const data = await response.json();
         setQuestions(data);
-    } else {
+      } else {
         console.error("Failed to fetch data");
-    }
+      }
     } catch {
-        console.error("Error while fetching data:", error);
+      console.error("Error while fetching data:", error);
     }
+  };
+
+  useEffect(() => {
+    fetchUserScore();
+  }, []);
+
+  return (
+    <div>
+      <h1>Personality Questionnaire</h1>
+      {/* <ul>{questions && questions.map((question, index) => <li key={index}>{question}</li>)}</ul> */}
+    </div>
+  );
 };
-
-    useEffect(()=>{
-        fetchUserScore();
-
-    },[]);
-    
-    
-    return (
-        <div>
-            <h1>Personality Questionnaire</h1>
-            <ul>
-            {questions.map((question, index) => (
-            <li key={index}>{question}</li>
-        ))}
-        </ul>
-        </div>
-    )
-}
 export default Option;
