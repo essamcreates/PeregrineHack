76a77c7b1ba7512c390a877f9011745b4bba1a20
essamcreates <mrm.hussain@hotmail.co.uk>
2023-10-19 17:08:16 +0100

    failing to fetch data

--- front-end-client/src/Components/QuizPage/Option.jsx
+++ front-end-client/src/Components/QuizPage/Option.jsx
@@ -1,14 +1,22 @@
 import {Link} from "react-router-dom";
-
+import { useState, useEffect } from "react";
 
 const Option = () => {
 
     const [questions, setQuestions] = useState([]);
-    const fetchUserScore = async () =>{
+    const fetchUserScore = async () => {
+        try {
         const response = await fetch('http://localhost:8080/personalityQuestionnaire');
+        if (response.ok) {
         const data = await response.json();
         setQuestions(data);
+    } else {
+        console.error("Failed to fetch data");
     }
+    } catch {
+        console.error("Error while fetching data:", error);
+    }
+};
 
     useEffect(()=>{
         fetchUserScore();
@@ -18,9 +26,12 @@ const Option = () => {
     
     return (
         <div>
+            <h1>Personality Questionnaire</h1>
+            <ul>
             {questions.map((question, index) => (
-            <div key={index}>{question}</div>
+            <li key={index}>{question}</li>
         ))}
+        </ul>
         </div>
     )
 }
