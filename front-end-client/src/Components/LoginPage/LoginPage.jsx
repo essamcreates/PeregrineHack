25d42b493ff3a94eb2ba695b475178298cce0b16
amelieboydshire <aboydshire10@gmail.com>
2023-10-19 18:56:07 +0100

    added background styling for loginPage

--- front-end-client/src/Components/LoginPage/LoginPage.jsx
+++ front-end-client/src/Components/LoginPage/LoginPage.jsx
@@ -8,18 +8,20 @@ const LoginPage = ({authenticateUser, currentUser, setCurrentUser}) => {
     const navigate = useNavigate();
 
     return (
-        <div class="min-h-screen flex items-center justify-center bg-stone-100">
-  <div class="bg-white shadow-lg rounded-lg h-85 p-10">
+     <div class="min-h-screen bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-emerald-200 via-cyan-400 to-yellow-100">
+        <h1 class="flex justify-center font-bold text-6xl pt-10">Rise and shine, it's time to grind!</h1>
+        <div class="min-h-screen flex items-center justify-center">
+   <div class="bg-white shadow-lg rounded-lg h-90 p-10">
     {!currentUser && (
       <>
         {/* <h1>User Login</h1> */}
-        <h1 class="text-2x1 mb-10">Welcome to your personal development coach</h1>
+        <h2 class="text-2x1 mb-10 white text-center text-lg font-medium">Welcome back</h2>
         <LoginForm authenticateUser={authenticateUser} currentUser={currentUser} setCurrentUser={setCurrentUser} />
       </>
     )}
     {currentUser && (
       <>
-        <h1>Sign Out</h1>
+        <h3>Sign Out</h3>
         <LoginForm authenticateUser={authenticateUser} currentUser={currentUser} setCurrentUser={setCurrentUser} />
       </>
     )}
@@ -31,6 +33,8 @@ const LoginPage = ({authenticateUser, currentUser, setCurrentUser}) => {
     </div>
   </div>
 </div>
+</div>
+
 
     )
 }
