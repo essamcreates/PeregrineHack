41e3e1cedd3431c2369ce39702e51872198dfcc5
charlotteg1 <charlotte.gould3@gmail.com>
2023-10-19 09:41:11 +0100

    added date and welcome user name to the home page, altered nav dock css

--- front-end-client/src/Components/HomePage/DailyMessage.jsx
+++ front-end-client/src/Components/HomePage/DailyMessage.jsx
@@ -1,7 +1,9 @@
 const DailyMessage = () => {
 
     return (
-        <></>
+        <>
+        <h2>Daily Message</h2>
+        </>
     )
 }
 export default DailyMessage;
--- front-end-client/src/Components/NavDock/NavDock.jsx
+++ front-end-client/src/Components/NavDock/NavDock.jsx
@@ -16,16 +16,23 @@ const NavDock = ({currentUser}) => {
     return (
         <div className="nav-dock">
             {currentUser && ( <div>
-                {!navOpen? (<button className="nav-open-menu-button" onClick={()=> setNavOpen(!navOpen) }>Nav Dock</button>) : (<button className="nav-close-menu-button" onClick={()=> setNavOpen(!navOpen) }>Close Dock</button>)}
+                {!navOpen? (<button className="nav-open-menu-button" onClick={()=> setNavOpen(!navOpen)}></button>) : (<button className="nav-close-menu-button" onClick={()=> setNavOpen(!navOpen) }></button>)}
 
                 {navOpen &&(
                     <div className="nav-dock-modal">
                         <div className="nav-dock-modal-content">
+                        <div className="empty-cell-1"></div>
+                        <div className="empty-cell-2"></div>
+                        <div className="empty-cell-3"></div>
+                        <div className="empty-cell-4"></div>
                         {location.pathname!=="/HomePage" && (<div>
-                            <button className="nav-icon-button" data-id="Home" onClick={()=>{setNavOpen(false);navigate("/HomePage")}}>🏠</button>
+                            <button className="nav-icon-button" data-id="Home" onClick={()=>{setNavOpen(false);navigate("/HomePage")}}>🏠</button> {/* find icons later*/}
                         </div>)}
-                        {location.pathname!=="/ProfilePage" && (<div>
-                            <button className="nav-icon-button" data-id="UserProfile" onClick={()=>{setNavOpen(false);navigate("/ProfilePage")}}>👥</button>
+                        {location.pathname!=="/ProfilePage" && (<div> 
+                            <button className="nav-icon-button" data-id="Profile" onClick={()=>{setNavOpen(false);navigate("/ProfilePage")}}>👥</button>
+                        </div>)}
+                        {location.pathname!=="/ProfileCreationPage" && (<div>
+                            <button className="nav-icon-button" data-id="Edit Info" onClick={()=>{setNavOpen(false);navigate("/ProfileCreationPage")}}>📝</button>
                         </div>)}
                         {location.pathname!=="/LoginPage" && (<div>
                             <button className="nav-icon-button" data-id="Signout" onClick={()=>{setNavOpen(false);navigate("/LoginPage")}}>👋🏼</button>
