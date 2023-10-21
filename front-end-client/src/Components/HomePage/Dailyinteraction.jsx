07da926bc30e35c3f2eb7319724346a04ed2df11
charlotteg1 <charlotte.gould3@gmail.com>
2023-10-21 18:59:27 +0100

    functionality for daily question

--- front-end-client/src/Components/HomePage/Dailyinteraction.jsx
+++ front-end-client/src/Components/HomePage/Dailyinteraction.jsx
@@ -7,13 +7,8 @@ const DailyInteraction = ({currentUser}) => {
     // display options to user
 
     const [question, setQuestion] = useState();
-
-    // "question": "How would you describe your day so far?",
-    // "optionOne": "Excellent",
-    // "optionTwo": "Terrible",
-    // "optionThree": "Average",
-    // "optionFour": "Ok",
-    // "optionFive": null
+    const [questionAnswered, setQuestionAnswered] = useState(false)
+    const [userAnswer, setUserAnswer] = useState();
 
     const fetchQuestion = async (id) => {
         try {
@@ -32,23 +27,64 @@ const DailyInteraction = ({currentUser}) => {
     };
 
     useEffect(()=>{
-        // returns a random number between 1 and 2
-        fetchQuestion(Math.floor((Math.random() * 2) + 1))
+        if( !question){
+        // returns a random number between 1 and 2 (to be increase once data loader is done)
+        fetchQuestion(Math.floor((Math.random() * 2) + 1))}
     },[])
 
+    const postQuestionAnswer = async(info) => {
+        const url = `http://localhost:8080/answeredQuestions`;
+        await fetch(url, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(info),
+        })
+    }
 
-    // const displayOptions = () => {
-    //     for
-    // }
+    const handleQuestionSubmit = (answer) => {
+        setQuestionAnswered(true)
+        setUserAnswer(answer)
+        let template = {
+                "chosenOption": answer,
+                "dailyQuestionId": question.id, 
+                "userId": currentUser.id
+        }
+        postQuestionAnswer(template)
+
+    }
+
+
+    const displayOptions = () => {
+        const options=["optionOne","optionTwo","optionThree","optionFour","optionFive"]
+        const choices=[]
+        options.map((option)=> {
+            const choice = question[option]
+            if(choice){
+                choices.push(
+                <div>
+                    <button value={option} onClick={(e)=>{handleQuestionSubmit(e.target.value)}}>{choice}</button>
+                </div>
+                )
+            }
+        });
+        return choices
+    }
 
     return (
         <>
         <div><h3>Question</h3></div>
-        {question && (<div>
-            {question.question}
-            {/* {displayOptions} */}
-
-        </div>)}
+        {question && !questionAnswered &&(
+            <div>
+                {question.question}
+                {displayOptions()}
+            </div>
+        )}
+        {questionAnswered && (
+            <div>
+                <p>Question: {question.question}</p>
+                <p>Your Answer: {question[userAnswer]}</p>
+            </div>
+        )}
         </>
     )
 }
