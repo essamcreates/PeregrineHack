db1bb3da0b09c5aded2d8f249a97d9435f4ce73b
Callum BP <122090016+cbattenplowright@users.noreply.github.com>
2023-10-21 17:41:09 +0100

    fixed tailwind not working was missing \'**\' in the src path in tailwind.config.js

--- front-end-client/src/Components/LoginPage/LoginPage.jsx
+++ front-end-client/src/Components/LoginPage/LoginPage.jsx
@@ -1,41 +1,54 @@
 import { useNavigate } from "react-router-dom";
 import LoginForm from "./LoginForm";
-// import './LoginPage.css';
+// import "./LoginPage.css";
 
+const LoginPage = ({ authenticateUser, currentUser, setCurrentUser }) => {
+  const navigate = useNavigate();
 
-const LoginPage = ({authenticateUser, currentUser, setCurrentUser}) => {
+  return (
+    <div className="min-h-screen bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-emerald-200 via-cyan-400 to-yellow-100">
+      <h1 className="flex justify-center font-bold text-6xl pt-10">
+        Rise and shine, it's time to grind!
+      </h1>
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="bg-white shadow-lg rounded-lg h-90 p-10">
+          {!currentUser && (
+            <>
+              {/* <h1>User Login</h1> */}
+              <h2 className="text-2x1 mb-10 white text-center text-lg font-medium">Welcome back</h2>
+              <LoginForm
+                authenticateUser={authenticateUser}
+                currentUser={currentUser}
+                setCurrentUser={setCurrentUser}
+              />
+            </>
+          )}
+          {currentUser && (
+            <>
+              <h3>Sign Out</h3>
+              <LoginForm
+                authenticateUser={authenticateUser}
+                currentUser={currentUser}
+                setCurrentUser={setCurrentUser}
+              />
+            </>
+          )}
 
-    const navigate = useNavigate();
-
-    return (
-     <div class="min-h-screen bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-emerald-200 via-cyan-400 to-yellow-100">
-        <h1 class="flex justify-center font-bold text-6xl pt-10">Rise and shine, it's time to grind!</h1>
-        <div class="min-h-screen flex items-center justify-center">
-   <div class="bg-white shadow-lg rounded-lg h-90 p-10">
-    {!currentUser && (
-      <>
-        {/* <h1>User Login</h1> */}
-        <h2 class="text-2x1 mb-10 white text-center text-lg font-medium">Welcome back</h2>
-        <LoginForm authenticateUser={authenticateUser} currentUser={currentUser} setCurrentUser={setCurrentUser} />
-      </>
-    )}
-    {currentUser && (
-      <>
-        <h3>Sign Out</h3>
-        <LoginForm authenticateUser={authenticateUser} currentUser={currentUser} setCurrentUser={setCurrentUser} />
-      </>
-    )}
-
-    <div class="w-full flex justify-center">
-    {!currentUser && (
-      <button class="mb--3" onClick={() => {navigate("/AccountRegistrationPage")}}>Don't have an account? Sign up</button>
-    )}
+          <div className="w-full flex justify-center">
+            {!currentUser && (
+              <button
+                className="mb--3"
+                onClick={() => {
+                  navigate("/AccountRegistrationPage");
+                }}
+              >
+                Don't have an account? Sign up
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-</div>
-
-
-    )
-}
+  );
+};
 export default LoginPage;
