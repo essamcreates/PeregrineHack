8a948b9896c3e95b7501d95b3b93d4a88a9e9829
charlotteg1 <charlotte.gould3@gmail.com>
2023-10-22 16:32:59 +0100

    styling to send button and include button colour change on hover

--- front-end-client/src/Components/HomePage/ChatBot.jsx
+++ front-end-client/src/Components/HomePage/ChatBot.jsx
@@ -121,7 +121,7 @@ const ChatBot = () => {
                     <textarea class="border-2 border-blue-800 bg-green-200 w-3/4 h-20 mt-3 p-1 rounded-lg" placeholder="Ask Farai about career goals, skills, wellness..." type="text" maxlength="150" value={userInput} onChange={(e)=>setUserInput(e.target.value)}/>
                 </div>
                 <div class="h-full flex justify-end">
-                        <button onClick={()=>{chatBotRequest(userInput);setPrevChoices((prevChoices) => [...prevChoices, userInput]) ;setSent(true)}}>Ask Farai</button>
+                        <button class="w-1/5 h-full mt-1 border-2 border-blue-900 bg-teal-200 p-1 text-center rounded-md transition-transform transform hover:bg-teal-300" onClick={()=>{chatBotRequest(userInput);setPrevChoices((prevChoices) => [...prevChoices, userInput]) ;setSent(true)}}>Ask Farai</button>
                 </div>
                 </>)}
                 </div>)
@@ -130,9 +130,9 @@ const ChatBot = () => {
         const choices =[]
         currentLevel.options.map((option, index)=>{
             if((index=== currentLevel.options.length -1) && (index % 2===0)){
-                choices.push(<div class="col-span-2 flex items-center justify-center m-1" key={index}><button class="w-11/12 h-full mt-3 m-2 border-2 border-blue-900 bg-red-200 p-1 text-center rounded-md"onClick={()=>{handleOptionClick(option.displayText,option.sendText,option.next)}}>{option.displayText}</button></div>) 
+                choices.push(<div class="col-span-2 flex items-center justify-center m-1" key={index}><button class="w-11/12 h-full mt-3 m-2 border-2 border-blue-900 bg-red-200 p-1 text-center rounded-md transition-transform transform hover:bg-red-300"onClick={()=>{handleOptionClick(option.displayText,option.sendText,option.next)}}>{option.displayText}</button></div>) 
             }else{
-            choices.push(<div class=" flex items-center justify-center m-1" key={index}><button class="w-10/12 h-full m-2 mt-2 border-2 border-blue-900 bg-red-200 p-1 text-center rounded-md"onClick={()=>{handleOptionClick(option.displayText,option.sendText,option.next)}}>{option.displayText}</button></div>)
+            choices.push(<div class=" flex items-center justify-center m-1" key={index}><button class="w-10/12 h-full m-2 mt-2 border-2 border-blue-900 bg-red-200 p-1 text-center rounded-md transition-transform transform hover:bg-red-300"onClick={()=>{handleOptionClick(option.displayText,option.sendText,option.next)}}>{option.displayText}</button></div>)
             }
         })
         return (<div class="place-items-start">
@@ -157,13 +157,13 @@ const ChatBot = () => {
                 <p class="text-sm ml-1 mb-0">Farai</p>
             {prevText.message && (<div><p class="ml-1 border-2 border-blue-900 bg-blue-100 w-3/4 p-2 text-center rounded-md ">{prevText.message}</p></div>)}
             <div class="grid grid-cols-2 w-3/4 m-1">{choices}</div>
-            <div class="flex justify-end">
-                <p class="text-sm mr-1 mb-0">You</p></div>
-            <div class="flex justify-end">
-                <p class="mr-1 w-2/5 border-2 border-blue-900 bg-green-100 p-1 text-center rounded-md">{prevChoices[index]}</p>
-            </div>
-        </div>)
-        })
+                <div class="flex justify-end">
+                    <p class="text-sm mr-1 mb-0">You</p></div>
+                <div class="flex justify-end">
+                    <p class="mr-1 w-2/5 border-2 border-blue-900 bg-green-100 p-1 text-center rounded-md">{prevChoices[index]}</p>
+                </div>
+            </div>)
+             })
         if(currentStep===100 && sent){
             return(<>
                 {prev}
