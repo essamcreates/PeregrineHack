ae3dba1d2910e3ff53f31467e49ce57cf2a8e49d
charlotteg1 <charlotte.gould3@gmail.com>
2023-10-23 15:45:46 +0100

    added ai request to wellness and resources boxes and can view on click

--- front-end-client/src/Components/HomePage/ResourcesBox.jsx
+++ front-end-client/src/Components/HomePage/ResourcesBox.jsx
@@ -1,8 +1,52 @@
+import { useState, useEffect } from "react";
+
 const ResourcesBox = () => {
+
+    const [userResources, setUserResources] = useState();
+    const [requestedCareerResources , setRequestedCareerResources] = useState(false);
+
+
+    const resourcesRequest = async()=>{
+        try {
+            const url = `http://localhost:8080/openAI`;
+            const response = await fetch(url, {
+              method: "POST",
+              headers: { "Content-Type": "text/plain" },
+              body: "give me 3 resources with links that can help me improve at work , give them in a format of an object with an array link [description, link] for js. Here is my info career goals: leadership and you may want to take in my needs like ADHD, but not all resources should be focused on this",
+            });
+            
+        
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+        
+            const data = await response.text();
+        
+            // Set the response data in the component's state
+            setUserResources(data);
+            console.log(data)
+          } catch (error) {
+            console.error('There was a problem with the fetch operation:', error);
+          }
+       
+    }
+
+    useEffect(()=>{
+        if(requestedCareerResources){
+        resourcesRequest()}
+    },[requestedCareerResources])
 
     return (
         <>
-        <h2>My Resources</h2>
+        <div class="border-2 border-slate-700 bg-blue-50 h-full rounded-lg p-1 shadow-xl">
+            <h2 class="text-2xl pt-3">My Resources</h2>
+            {!userResources && (<button class="border-1 border-grey-200" onClick={()=>{setRequestedCareerResources(true)}}>Click to see your resources</button>)}
+            {userResources && requestedCareerResources && (
+                <div>
+                    {userResources}
+                </div>
+            )}
+        </div>
         </>
     )
 }
--- front-end-client/src/Components/HomePage/WellnessBox.jsx
+++ front-end-client/src/Components/HomePage/WellnessBox.jsx
@@ -1,8 +1,54 @@
+import { useState, useEffect } from "react";
+
 const WellnessBox = () => {
+
+    const [userWellnessResources, setUserWellnessResources] = useState();
+    const [requestedResources, setRequestedResources] = useState(false)
+
+
+    const wellnessResourcesRequest = async()=>{
+        try {
+            const url = `http://localhost:8080/openAI`;
+            const response = await fetch(url, {
+              method: "POST",
+              headers: { "Content-Type": "text/plain" },
+              body: "give me 3 resources with links that can help me improve my wellness, array format js ",
+            });
+            // put into request, give them in a format of an object with an array link [description, link] for js. Here is my info you may want to take in my needs like ADHD and ocd, but not all resources should be focused on this
+            
+        
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+        
+            const data = await response.text();
+        
+            // Set the response data in the component's state
+            setUserWellnessResources(data);
+            console.log(data)
+          } catch (error) {
+            console.error('There was a problem with the fetch operation:', error);
+          }
+       
+    }
+
+    // need to add time delay as two requests to open ai at one time
+    useEffect(()=>{
+        if(requestedResources){
+        wellnessResourcesRequest()}
+    },[requestedResources])
 
     return (
         <>
-        <h2>Wellness Resources</h2>
+        <div class="border-2 border-slate-700  bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-yellow-400 via-amber-300 to-green-100 h-full rounded-lg p-1 shadow-xl">
+            <h2 class="text-2xl pt-3">Wellness Resources</h2>
+            {!userWellnessResources && (<button class="border-1 border-slate-300 " onClick={()=>{setRequestedResources(true)}}>Click to see wellness resources</button>)}
+            {userWellnessResources && requestedResources && (
+                <div>
+                    {userWellnessResources}
+                </div>
+            )}
+        </div>
         </>
     )
 }
