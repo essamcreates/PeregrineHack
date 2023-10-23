15bb50715563c46c38fb234d35829936243b1e8e
amelieboydshire <aboydshire10@gmail.com>
2023-10-23 17:06:26 +0100

    re-positioned text in LoginPage

--- front-end-client/src/Components/LoginPage/LoginPage.jsx
+++ front-end-client/src/Components/LoginPage/LoginPage.jsx
@@ -8,65 +8,58 @@ const LoginPage = ({authenticateUser, currentUser, setCurrentUser}) => {
     const navigate = useNavigate();
 
     return (
-     <div class="min-h-screen bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-emerald-200 via-cyan-400 to-yellow-100">
-        <div class="grid grid-cols-2 content-center">
-            {/* column one */}
-  <div>
-        <h1 class="flex font-bold text-6xl pt-10 opacity-40 line-through text-left">Log In</h1>
-        <h1 class="flex font-bold text-6xl pt-5 opacity-50 text-left">Clock In.</h1>
-        
- <div class="min-h-screen flex items-center justify-center">
-   <div class="bg-white shadow-lg rounded-lg box-content w-96 h-96x p-20 ml-10 mb-20">
-    {!currentUser && (
-      <>
-        {/* <h1>User Login</h1> */}
-        <h2 class="text-2x1 mb-10 white text-center text-2xl font-medium pb-10">Welcome back</h2>
-        <LoginForm authenticateUser={authenticateUser} currentUser={currentUser} setCurrentUser={setCurrentUser} />
-      </>
-    )}
-    {currentUser && (
-      <>
-        <h3>Sign Out</h3>
-        <LoginForm authenticateUser={authenticateUser} currentUser={currentUser} setCurrentUser={setCurrentUser} />
-      </>
-    )}
-    <div class="w-full flex justify-center">
-    {!currentUser && (
-      <button class="mb--3" onClick={() => {navigate("/AccountRegistrationPage")}}>Don't have an account? <span class="underline">Sign Up</span></button>
- )}
+      <div class="min-h-screen bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-emerald-200 via-cyan-400 to-yellow-100">
+      <div class="grid grid-cols-2">
+        {/* Column one */}
+        <div class="flex flex-col justify-center p-20 ml-10">
+          <div class="min-h-screen flex items-center justify-center">
+            <div class="bg-white shadow-lg rounded-lg w-[550px] h-96x p-0 ml-10 mb-20">
+              <h1 class="font-bold text-6xl opacity-40 line-through text-left ml-5 pt-5">
+                Log In
+              </h1>
+              <h1 class="font-bold text-6xl opacity-50 text-left ml-5 pt-2">
+                Clock In.
+              </h1>
+              {!currentUser && (
+                <div class="p-10">
+                  <h2 class="text-2x1 mb-10 white text-center text-2xl font-medium pb-10 pt-2">Welcome back</h2>
+                  <LoginForm authenticateUser={authenticateUser} currentUser={currentUser} setCurrentUser={setCurrentUser} />
+                </div>
+              )}
+              {currentUser && (
+                <div class="p-10">
+                  <LoginForm authenticateUser={authenticateUser} currentUser={currentUser} setCurrentUser={setCurrentUser} />
+                </div>
+              )}
+              <div class="w-full flex justify-center">
+                {!currentUser && (
+                  <button class="mb-7" onClick={() => {navigate("/AccountRegistrationPage")}}>
+                    Don't have an account? <span class="underline">Sign Up</span>
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Column two */}
+        <div class="flex justify-center items-center pr-20">
+          <h1 class="flex-items-center text-6xl font-medium text-neutral-400">
+            Dive Into
+          </h1>
+          <span class="relative ml-3 h-[1em] w-36 text-6xl overflow-y-hidden">
+            <span class="absolute h-full w-full -translate-y-full animate-slide leading-none text-white">
+              <p>Ideas</p>
+            </span>
+            <span class="absolute h-full w-full -translate-y-full animate-slide leading-none text-white [animation-delay:0.8s]">
+              <p>Calm</p>
+            </span>
+            <span class="absolute h-full w-full -translate-y-full animate-slide leading-none text-white [animation-delay:1.6s]">
+              <p>Goals</p>
+            </span>
+          </span>
+        </div>
+      </div>
     </div>
-    </div>
-    </div>
-    </div>
-    {/* column two */}
-    <div class="flex justify-center items-center pr-20"> 
-    {/* <div class="absolute top-10 bottom-5 right-7"> */}
-        <h1 class="flex-items-center text-6xl font-medium text-neutral-400">
-  Dive Into</h1>
-  <span class="relative ml-3 h-[1em] w-36  text-6xl overflow-hidden">
-    <span
-      class="absolute h-full w-full -translate-y-full animate-slide leading-none text-white "
-    >
-      <p>Ideas</p>
-    </span>
-    <span
-      class="absolute h-full w-full -translate-y-full animate-slide leading-none text-white [animation-delay:0.8s]"
-    >
-      <p>Tech</p>
-    </span>
-    <span
-      class="absolute h-full w-full -translate-y-full animate-slide leading-none text-white  [animation-delay:1.6s]"
-    >
-      <p>Art</p>
-    </span>
-  </span>
-</div>
-
-   
-</div>
-</div>
-// </div>
-
 
 
     )
--- front-end-client/tailwind.config.js
+++ front-end-client/tailwind.config.js
@@ -1,55 +1,43 @@
-// <<<<<<< style
-// // /** @type {import('tailwindcss').Config} */
-// // export default {
-// //   content: [
-// //     './src/**/*.html',
-// //     './src/**/*.vue',
-// //     './src/**/*.jsx',
-// //     // Add any other file types or paths you want to include.
-// //   ],
-// //   theme: {
-// //     extend: {},
-// //   },
-// //   plugins: [],
-// // }
-// module.exports = {
-// =======
-// export default {
-// >>>>>>> charlotte-style
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
-    './src/**/*.html',
-    './src/**/*.vue',
-    './src/**/*.jsx',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
     // Add any other file types or paths you want to include.
   ],
   theme: {
-// <<<<<<< style
-//     extend: {
-//       animation: {
-//         slide: "slide 2.5s linear infinite",
-//       },
-//       keyframes: {
-//         slide: {
-//           "0%": { transform: "translateY(100%)", opacity: 0.1 },
-//           "15%": { transform: "translateY(0)", opacity: 1 },
-//           "30%": { transform: "translateY(0)", opacity: 1 },
-//           "45%": { transform: "translateY(-100%)", opacity: 1 },
-//           "100%": { transform: "translateY(-100%)", opacity: 0.1 },
-//         },
-//       },
-//     },
-//   },
-//   variants: {
-//     extend: {},
-//   },
-//   plugins: [],
-// };
-//   // Other Tailwind CSS configuration options...
-
-// =======
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-// >>>>>>> charlotte-style
-
+    extend: {
+      colors: {
+        primary: {
+          50: "#ecfeff",
+          100: "#cffafe",
+          200: "#a5f3fc",
+          300: "#67e8f9",
+          400: "#22d3ee",
+          500: "#06b6d4",
+          600: "#0891b2",
+          700: "#0e7490",
+          800: "#155e75",
+          900: "#164e63",
+          950: "#083344"
+        }
+      },
+      animation: {
+        slide: "slide 2.5s linear infinite"
+      },
+      keyframes: {
+        slide: {
+          "0%": { transform: "translateY(100%)", opacity: 0.1 },
+          "15%": { transform: "translateY(0)", opacity: 1 },
+          "30%": { transform: "translateY(0)", opacity: 1 },
+          "45%": { transform: "translateY(-100%)", opacity: 1 },
+          "100%": { transform: "translateY(-100%)", opacity: 0.1 }
+        }
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
