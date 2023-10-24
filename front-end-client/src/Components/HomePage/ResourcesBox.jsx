dad3078330c06d3d4c32a81dc731cba765b476c8
charlotteg1 <charlotte.gould3@gmail.com>
2023-10-24 12:13:20 +0100

    refines the resource links that are given to user

--- front-end-client/src/Components/HomePage/ResourcesBox.jsx
+++ front-end-client/src/Components/HomePage/ResourcesBox.jsx
@@ -28,7 +28,7 @@ const ResourcesBox = ({currentUser}) => {
             const response = await fetch(url, {
               method: "POST",
               headers: { "Content-Type": "text/plain" },
-              body: "give me 3 resources with a short description and link that can help me improve at work. Here is my info career goals: leadership and you may want to take in my needs like ADHD, but not all resources should be focused on this ",
+              body: "give me 3 resources with a title and link (that works) that can help me improve at work, Here is my info career goals: leadership and you may want to take in my needs like ADHD, but not all resources should be focused on this. format like this  -. Use havards development course to improve leadership: www.harvard.com -. ..",
             });
             
         
@@ -58,10 +58,13 @@ const ResourcesBox = ({currentUser}) => {
           const parts = line.split(': ');
           if (parts.length === 2) {
             const description = parts[0].trim();
-            const link = parts[1].trim();
+            let link = parts[1].trim();
+            if (!link.startsWith('http')) {
+                link = 'https://' + link;
+            }
             // is the link and the box it is in , taget and rel allow for the link to open in a new tab
-            resourceList.push(<li class="font-serif w-3/4 m-3 text-center border-2 border-teal-200 bg-neutral-50 text-black hover:bg-slate-300 p-4 rounded-lg shadow-lg" key={index} >
-                    <a  href={link}  target="_blank" rel="noopener noreferrer">
+            resourceList.push(<li class="font-serif w-3/4 m-3 text-center text-md border-2 border-teal-100 bg-neutral-50 text-black hover:bg-slate-300 p-4 rounded-lg shadow-lg" key={index} >
+                    <a  href={link} target="_blank" rel="noopener noreferrer">
                         {description}
                     </a>
                 </li>
@@ -75,11 +78,10 @@ const ResourcesBox = ({currentUser}) => {
         <>
         <div class="border-2 border-slate-700 bg-blue-50 h-full rounded-lg p-1 shadow-xl">
             <h2 class="text-2xl pt-3">Career Resources</h2>
-            {!userResources && (<div class="flex items-center justiy-center mt-6"><p class="border-1 border-grey-200 bg-teal-100 h-1/2 w-full m-3 text-center text-2xl rounded-lg">Loading your tailored resources  <span class="animate-pulse">....</span>  </p></div>)}
+            {!userResources && (<div class="flex items-center justiy-center mt-6"><p class="border-1 border-grey-200 bg-neutral-50 h-1/2 w-full m-3 text-center text-2xl rounded-lg">Loading your tailored career resources  <span class="animate-pulse">....</span>  </p></div>)}
             <div>
                 {userResources && (<>
-                    <p class="font-serif p-3 text-md mb-5 mt-3 text-center">Please click on these links to view your personalised resources, which have been tailored to suit your needs.......</p>
-                    <div class="flex items-center justify-center h-full">
+                    <div class="flex items-center justify-center h-full mt-3">
                         <ul class="flex flex-col items-center w-full h-full ">
                         {careerResources()}
                         </ul>
--- front-end-client/src/Components/HomePage/WellnessBox.jsx
+++ front-end-client/src/Components/HomePage/WellnessBox.jsx
@@ -25,7 +25,7 @@ const WellnessBox = ({currentUser}) => {
 
     const wellnessResourcesRequest = async()=>{
         try {
-            const request = "give me 3 resources with a short description and link that can help me improve my wellness, "
+            const request = "(note im in uk) give me 3 resources with a title and link (that works) that can help me improve my wellness, example -. use mindtools to help improve your mental health : www.mindtools.com -. very short description : link"
             // map through accessNeeds and mental health conditions
             // format an comma between 
             // add at end but not all resources should be focused on this
@@ -61,9 +61,12 @@ const WellnessBox = ({currentUser}) => {
           const parts = line.split(': ');
           if (parts.length === 2) {
             const description = parts[0].trim();
-            const link = parts[1].trim();
+            let link = parts[1].trim();
+            if (!link.startsWith('http')) {
+                link = 'https://' + link;
+            }
             // is the link and the box it is in , taget and rel allow for the link to open in a new tab
-            resourceList.push(<li class="font-serif w-3/4 m-3 text-center border-2 border-teal-200 bg-neutral-50 text-black hover:bg-slate-300 p-4 rounded-lg shadow-lg" key={index} >
+            resourceList.push(<li class="font-serif w-3/4 m-3 text-center border-2 border-teal-100 bg-neutral-50 text-black hover:bg-slate-300 p-4 rounded-lg shadow-lg" key={index} >
                     <a  href={link}  target="_blank" rel="noopener noreferrer">
                         {description}
                     </a>
@@ -71,6 +74,7 @@ const WellnessBox = ({currentUser}) => {
                 );
           }
         });
+        console.log(resourceList[1])
         return resourceList;
       };
 
@@ -78,12 +82,10 @@ const WellnessBox = ({currentUser}) => {
         <>
         <div class="border-2 border-slate-700  bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-yellow-400 via-amber-300 to-green-100 h-full rounded-lg p-1 shadow-xl">
             <h2 class="text-2xl pt-3">Wellness Resources</h2>
-            {!userWellnessResources &&(<p class="border-1 border-slate-300 bg-slate-300 h-1/2 w-full text-center">Loading you tailored wellness resources <span class="animate-pulse">....</span> </p>)}
+            {!userWellnessResources &&(<div class="flex items-center justiy-center mt-6"><p class="border-1 border-slate-300 h-1/2 w-full bg-neutral-100 m-3 text-center text-2xl rounded-lg">Loading you tailored wellness resources <span class="animate-pulse">....</span> </p></div>)}
             <div>
             {userWellnessResources &&  (<>
-            
-            <p class="font-serif p-3 text-md mb-5 mt-3 text-center">Please click on these links to view your personalised resources, which have been tailored to suit your needs.......</p>
-                <div class="flex items-center justify-center h-full">
+                <div class="flex items-center justify-center h-full mt-3">
                     <ul class="flex flex-col items-center w-full h-full ">
                     {resources()}
                     </ul>
