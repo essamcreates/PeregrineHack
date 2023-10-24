28316ceb19af9e603d188fff13a52c98d58f03f4
Jannah Anwar <jannah.anwar@outlok.com>
2023-10-25 00:39:23 +0100

    playing with side bar

--- front-end-client/src/Components/HomePage/HomePage.jsx
+++ front-end-client/src/Components/HomePage/HomePage.jsx
@@ -1,20 +1,18 @@
-import { Link } from "react-router-dom";
-import "./HomePage.css";
+import React, { useEffect, useState } from "react";
 import DailyInteraction from "./DailyInteraction";
 import DailyMessage from "./DailyMessage";
 import WellnessBox from "./WellnessBox";
 import ResourcesBox from "./ResourcesBox";
 import ChatBot from "./ChatBot";
-import { useEffect, useState } from "react";
 import MoodEntry from "./MoodEntry";
 import DigitalClock from "./DigitalClock";
+import NoteTaking from "./NoteTaking";
 
 const HomePage = ({ currentUser }) => {
   const [currentDate, setCurrentDate] = useState("");
 
   const formatDate = (date) => {
-    const day = date.getDate(); // .getDate returns the day of the month
-    // to get suffix of day
+    const day = date.getDate();
     let suffix =
       day === 1 || day === 21 || day === 31
         ? "st"
@@ -38,46 +36,50 @@ const HomePage = ({ currentUser }) => {
 
   return (
     <div>
-      <div>
-        <section className="relative block h-[40vh] bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-orange-300 via-fuchsia-200 to-emerald-200">
-          <div className="relative top-0 w-full h-full flex justify-between items-center">
-            <div className="daily-message-box ml-12">
-              <DailyMessage />
-            </div>
-            <div>
-              <div className="flex flex-col items-centre mr-6">
-                <div className="mr-6">
-                  <DigitalClock />
-                </div>
-                {currentDate && (
-                  <div className="date-time whitespace-nowrap opacity-50">
-                    <p>{currentDate}</p>
-                  </div>
-                )}
+      <section className="relative block h-[40vh] bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-orange-400 via-red-200 to-pink-400">
+        <div className="relative top-0 w-full h-full flex justify-between items-center">
+          <div className="daily-message-box ml-12">
+            <DailyMessage />
+          </div>
+          <div>
+            <div className="flex flex-col items-center mr-6">
+              <div className="mr-6">
+                <DigitalClock />
               </div>
+              {currentDate && (
+                <div className="date-time whitespace-nowrap opacity-50">
+                  <p>{currentDate}</p>
+                </div>
+              )}
             </div>
           </div>
-        </section>
-      </div>
-      <div className="welcome-home-page"></div>
-      <div className="dashboard">
-        <div className="daily-interaction-box">
-          <DailyInteraction currentUser={currentUser} />
         </div>
-        <div className="mood-entry-box">
-          <MoodEntry currentUser={currentUser} />
-        </div>
-        <div className="chatbot-box">
-          <ChatBot currentUser={currentUser} />
-        </div>
-        <div className="wellness-box">
-          <WellnessBox currentUser={currentUser} />
-        </div>
-        <div className="resource-box">
-          <ResourcesBox currentUser={currentUser} />
+      </section>
+      <div className="flex">
+        <div className="flex-grow">
+          <div className="welcome-home-page"></div>
+          <div className="dashboard">
+            <div className="daily-interaction-box">
+              <DailyInteraction currentUser={currentUser} />
+            </div>
+            <div className="mood-entry-box">
+              <MoodEntry currentUser={currentUser} />
+            </div>
+            <div className="chatbot-box">
+              <ChatBot currentUser={currentUser} />
+            </div>
+            <div className="wellness-box">
+              <WellnessBox currentUser={currentUser} />
+            </div>
+            <div className="resource-box">
+              <ResourcesBox currentUser={currentUser} />
+            </div>
+          </div>
+          <NoteTaking />
         </div>
       </div>
     </div>
   );
 };
+
 export default HomePage;
