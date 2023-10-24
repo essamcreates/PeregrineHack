d8f93470edef9a52ea00181f8a730d42689b84ec
TarekQMUL <t.u.ahmed@se21.qmul.ac.uk>
2023-10-24 16:22:13 +0100

    added placeholders where there isnt any info on the profile page

--- front-end-client/src/Components/ProfilePage/BigFiveDisplay.jsx
+++ front-end-client/src/Components/ProfilePage/BigFiveDisplay.jsx
@@ -1,36 +1,38 @@
 import { useEffect, useState } from "react";
 
 const BigFiveDisplay = ({ currentUser }) => {
-  const [userScore, setUserScore] = useState([]);
+  const [userScore, setUserScore] = useState(null);
   const fetchUserScore = async () => {
-    const response = await fetch("http://localhost:8080/personalityScores/user/" + currentUser.id);
-    const data = await response.json();
-    setUserScore(data);
-  };
+    try {
+      const response = await fetch('http://localhost:8080/personalityScores/user/' + currentUser.id);
+      const data = await response.json();
+      setUserScore(data);
+    } catch (error) {
+      console.error("Error fetching user score:", error);
+    }
+  }
 
   useEffect(() => {
     fetchUserScore();
   }, []);
 
+
   return (
     <>
       <h2 className="text-2xl font-semibold">Big Five Information</h2>
       <div className="mt-4">
-        <p>
-          <strong>Openness:</strong> {Math.round(userScore.openness)}
-        </p>
-        <p>
-          <strong>Conscientiousness:</strong> {Math.round(userScore.conscientiousness)}
-        </p>
-        <p>
-          <strong>Extraversion:</strong> {Math.round(userScore.extraversion)}
-        </p>
-        <p>
-          <strong>Agreeableness:</strong> {Math.round(userScore.agreeableness)}
-        </p>
-        <p>
-          <strong>Neuroticism:</strong> {Math.round(userScore.neuroticism)}
-        </p>
+        <p>Your Big Five:</p>
+        {userScore ? (
+            <div>
+              <p>Openness: {userScore.openness}</p>
+              <p>Conscientiousness: {userScore.conscientiousness}</p>
+              <p>Extraversion: {userScore.extraversion}</p>
+              <p>Agreeableness: {userScore.agreeableness}</p>
+              <p>Neuroticism: {userScore.neuroticism}</p>
+            </div>
+        ) : (
+            <p>Please complete your personality test.</p>
+        )}
       </div>
     </>
   );
