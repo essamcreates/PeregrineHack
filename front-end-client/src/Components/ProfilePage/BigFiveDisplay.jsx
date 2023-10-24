6e3b7fecd6ffb621272800a4d272ecb608d0cd68
Callum BP <122090016+cbattenplowright@users.noreply.github.com>
2023-10-24 18:49:51 +0100

    test

--- front-end-client/src/Components/ProfilePage/BigFiveDisplay.jsx
+++ front-end-client/src/Components/ProfilePage/BigFiveDisplay.jsx
@@ -15,23 +15,6 @@ const BigFiveDisplay = ({ currentUser }) => {
   return (
     <>
       <h2 className="text-2xl font-semibold">Big Five Information</h2>
-      <div className="mt-4">
-        <p>
-          <strong>Openness:</strong> {Math.round(userScore.openness)}
-        </p>
-        <p>
-          <strong>Conscientiousness:</strong> {Math.round(userScore.conscientiousness)}
-        </p>
-        <p>
-          <strong>Extraversion:</strong> {Math.round(userScore.extraversion)}
-        </p>
-        <p>
-          <strong>Agreeableness:</strong> {Math.round(userScore.agreeableness)}
-        </p>
-        <p>
-          <strong>Neuroticism:</strong> {Math.round(userScore.neuroticism)}
-        </p>
-      </div>
     </>
   );
 };
