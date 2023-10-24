f9a9af9924ceea5c497b5c7dd7272bfdf99b3d8e
essamcreates <mrm.hussain@hotmail.co.uk>
2023-10-24 18:22:38 +0100

    completed progress bar for quiz

--- front-end-client/src/Components/QuizPage/QuizPage.jsx
+++ front-end-client/src/Components/QuizPage/QuizPage.jsx
@@ -6,18 +6,11 @@ import { useEffect, useState } from "react";
 
 const QuizPage = () => {
     const [currentQuestionId, setCurrentQuestionId] = useState(42);
-    const [totalQuestions, setTotalQuestions] = useState(42);
 
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
             <h1 className="text-3x1 font-bold mb-4 text-center">Personality Questionnaire</h1>
-            <div className="container mx-auto bg-white shadow-lg p-6 rounded-lg">
-                <PersonalityAssessmentBar 
-                currentQuestionId={currentQuestionId}
-                totalQuestions={totalQuestions}
-                />
-            </div>
             <div className="container mx-auto bg-white shadow-lg p-6 rounded-lg">
                 <Questions
                 currentQuestionId={currentQuestionId}
