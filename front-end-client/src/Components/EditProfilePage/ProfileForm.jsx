590b6d6830fd407da7bde82d2d766e813774b254
TarekQMUL <t.u.ahmed@se21.qmul.ac.uk>
2023-10-24 22:18:50 +0100

    upload profile photo styled and also fetches updated user details on load

--- front-end-client/src/Components/EditProfilePage/ProfileForm.jsx
+++ front-end-client/src/Components/EditProfilePage/ProfileForm.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import DateOfBirthListbox from "./DateOfBirthListbox";
 
-const ProfileForm = ({ currentUser }) => {
+const ProfileForm = ({ currentUser, setCurrentUser }) => {
   // Add in validation when no current user that they need to login first
 
   // const [enteredJobRole, setEnteredJobRole] = useState ("")
