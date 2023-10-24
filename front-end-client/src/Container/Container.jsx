5a6b85e8848de0599f2809203448f33de9ad2f70
Callum BP <122090016+cbattenplowright@users.noreply.github.com>
2023-10-24 15:24:27 +0100

    fixed background bugs

--- front-end-client/src/Container/Container.jsx
+++ front-end-client/src/Container/Container.jsx
@@ -56,49 +56,44 @@ const Container = () => {
   };
 
   return (
-    <div className="100vh">
-      <BrowserRouter>
-        <NavDock currentUser={currentUser} />
-        <Routes>
-          <Route path="/HomePage" element={<HomePage currentUser={currentUser} />}></Route>
-          <Route
-            path="/LoginPage"
-            element={
-              <LoginPage
-                authenticateUser={authenticateUser}
-                currentUser={currentUser}
-                updateCurrentUser={updateCurrentUser}
-                setCurrentUser={setCurrentUser}
-              />
-            }
-          ></Route>
-          <Route
-            path="/AccountRegistrationPage"
-            element={<AccountRegistrationPage signupUser={signupUser} />}
-          ></Route>
-          <Route
-            path="/EditProfile"
-            element={<EditProfilePage currentUser={currentUser} />}
-          ></Route>
-          <Route
-            path="/ProfilePage"
-            element={<ProfilePage imageName={imageName} currentUser={currentUser} />}
-          ></Route>
-          <Route path="/QuizPage" element={<QuizPage />}></Route>
-          <Route
-            path="/UploadProfilePhoto"
-            element={
-              <UploadProfilePhoto
-                setImageName={setImageName}
-                imageName={imageName}
-                currentUser={currentUser}
-                setCurrentUser={setCurrentUser}
-              />
-            }
-          ></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <NavDock currentUser={currentUser} />
+      <Routes>
+        <Route path="/HomePage" element={<HomePage currentUser={currentUser} />}></Route>
+        <Route
+          path="/LoginPage"
+          element={
+            <LoginPage
+              authenticateUser={authenticateUser}
+              currentUser={currentUser}
+              updateCurrentUser={updateCurrentUser}
+              setCurrentUser={setCurrentUser}
+            />
+          }
+        ></Route>
+        <Route
+          path="/AccountRegistrationPage"
+          element={<AccountRegistrationPage signupUser={signupUser} />}
+        ></Route>
+        <Route path="/EditProfile" element={<EditProfilePage currentUser={currentUser} />}></Route>
+        <Route
+          path="/ProfilePage"
+          element={<ProfilePage imageName={imageName} currentUser={currentUser} />}
+        ></Route>
+        <Route path="/QuizPage" element={<QuizPage />}></Route>
+        <Route
+          path="/UploadProfilePhoto"
+          element={
+            <UploadProfilePhoto
+              setImageName={setImageName}
+              imageName={imageName}
+              currentUser={currentUser}
+              setCurrentUser={setCurrentUser}
+            />
+          }
+        ></Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 export default Container;
